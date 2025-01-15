6f555cbc95f6f19c755234dc84bb6081cbd4c7d7
borti4938 <borti4938@gmail.com>
2025-01-15 17:50:57 +0100

    Update CPLD firmware for use with 5M80ZE64 CPLD

--- fw/n64rgb_flex.v
+++ fw/n64rgb_flex.v
@@ -2,7 +2,7 @@
 //
 // This file is part of the N64 RGB/YPbPr DAC project.
 //
-// Copyright (C) 2015-2022 by Peter Bartmann <borti4938@gmail.com>
+// Copyright (C) 2015-2025 by Peter Bartmann <borti4938@gmail.com>
 //
 // N64 RGB/YPbPr DAC is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -34,6 +34,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 `define USE_POSEDGE_VCLK
+`define USE_INPUT_REGS
 
 module n64rgb_flex(
   VCLK_i,
@@ -61,6 +62,8 @@ wire VCLK_w;
 wire negedge_nVSYNC_w, negedge_nHSYNC_w, posedge_nCSYNC_w;
 
 //regs
+reg nDSYNC_L;
+reg [6:0] D_L;
 reg [3:0] SYNC_L;
 reg [6:0] R_L [0:1];
 reg [6:0] G_L [0:1];
@@ -75,19 +78,37 @@ reg [1:0] phase_cnt = 2'b00;
 reg nblank_vdata;
 
 // rtl
+
+// clock input
 `ifdef USE_POSEDGE_VCLK
   assign VCLK_w = VCLK_i;
 `else
   assign VCLK_w = ~VCLK_i;
 `endif
 
-assign negedge_nVSYNC_w =  SYNC_L[3] & !D_i[3];
-assign negedge_nHSYNC_w =  SYNC_L[1] & !D_i[1];
-assign posedge_nCSYNC_w = !SYNC_L[0] &  D_i[0];
+
+// input registers
+`ifdef USE_INPUT_REGS
+  always @(posedge VCLK_w) begin
+    nDSYNC_L <= nDSYNC_i;
+    D_L <= D_i;
+  end
+`else
+  always @(*) begin
+    nDSYNC_L <= nDSYNC_i;
+    D_L <= D_i;
+  end
+`endif
+
+
+// video decoder
+assign negedge_nVSYNC_w =  SYNC_L[3] & !D_L[3];
+assign negedge_nHSYNC_w =  SYNC_L[1] & !D_L[1];
+assign posedge_nCSYNC_w = !SYNC_L[0] &  D_L[0];
 
 
 always @(posedge VCLK_w)
-  if (!nDSYNC_i) begin
+  if (!nDSYNC_L) begin
     if(negedge_nVSYNC_w) begin // posedge at nVSYNC detected - reset line_cnt and set palmode
       line_cnt <= 2'b00;
       palmode <= ~line_cnt[1];
@@ -97,7 +118,7 @@ always @(posedge VCLK_w)
   end
   
 always @(posedge VCLK_w)
-  if (!nDSYNC_i) begin
+  if (!nDSYNC_L) begin
     if (negedge_nVSYNC_w) begin                 // negedge at nVSYNC
       field_id <= negedge_nHSYNC_w;             // if negedge at nHSYNC too, then odd field, otherwise even field
       n64_480i <= field_id ^ negedge_nHSYNC_w;  // n64_480i becomes high if field_id changes, otherwise it stays zero
@@ -105,7 +126,7 @@ always @(posedge VCLK_w)
   end
   
 always @(posedge VCLK_w)
-  if (!nDSYNC_i) begin
+  if (!nDSYNC_L) begin
     if (nViDeBlur_i | n64_480i) begin
       nblank_vdata <= 1'b1;
     end else begin
@@ -117,9 +138,9 @@ always @(posedge VCLK_w)
   end
 
 always @(posedge VCLK_w)
-  if (!nDSYNC_i) begin
+  if (!nDSYNC_L) begin
     phase_cnt <= 2'b00;
-    SYNC_L <= D_i[3:0];
+    SYNC_L <= D_L[3:0];
     if (nblank_vdata) begin
       R_L[1] <= R_L[0];
       G_L[1] <= G_L[0];
@@ -127,14 +148,16 @@ always @(posedge VCLK_w)
     end
   end else begin
     casez(phase_cnt)
-      2'b00: R_L[0] <= D_i;
-      2'b01: G_L[0] <= D_i;
-      2'b10: B_L[0] <= D_i;
+      2'b00: R_L[0] <= D_L;
+      2'b01: G_L[0] <= D_L;
+      2'b10: B_L[0] <= D_L;
     endcase
     phase_cnt <= phase_cnt + 2'b01;
   end
 
-always @(SYNC_L,R_L[1],G_L[1],B_L[1]) begin
+
+// setting outputs
+always @(*) begin
   nCSYNC_o <= SYNC_L[0];
   {R_o,G_o,B_o} <= {R_L[1],G_L[1],B_L[1]};
 end
